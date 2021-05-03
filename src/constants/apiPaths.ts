a62a580d6773f115de00ac67d2fe29bdb17e3060
Viktar_Parashchanka <viktar_parashchanka@epam.com>
2021-05-04 01:23:59 +0300

    Merge branch 'main' of https://github.com/scolt/nodejs-aws-fe into main
    # Conflicts:
    #	src/components/pages/PageProducts/components/productList.json

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://4pmxwjtxxa.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://j8euk8i90h.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
