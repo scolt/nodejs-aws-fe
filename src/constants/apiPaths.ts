1e5b3a4a030ef28af221ba6f7f62052cfe6ef78c
Viktar_Parashchanka <viktar_parashchanka@epam.com>
2021-05-18 13:59:42 +0300

    Update URLS

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://4pmxwjtxxa.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://k0i0cof1f2.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://j8euk8i90h.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://skwo66fjl6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
