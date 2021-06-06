21785a7e30dcc7a2a206e2f89ed48f6294367cad
Viktar_Parashchanka <viktar_parashchanka@epam.com>
2021-06-06 03:24:56 +0300

    Fix token

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,11 +31,12 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
+      const token = localStorage.getItem('authorization_token');
       const response = await axios({
         method: 'GET',
         url,
         headers: {
-          Authorization: `Basic ${localStorage.getItem('authorization_token')}`
+          Authorization: token ? `Basic ${token}` : '',
         },
         params: {
           name: encodeURIComponent(file.name)
