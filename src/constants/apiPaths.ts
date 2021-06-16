e3d9b7e567e35bd4f02bedc73d86bf84773d4ac8
Viktar_Parashchanka <viktar_parashchanka@epam.com>
2021-06-16 23:45:03 +0300

    AWS Task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://skwo66fjl6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://scolt-cart-api-development.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://scolt-cart-api-development.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
